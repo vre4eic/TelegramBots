dfec83921975753e59b086038bd826a0a17e2ae4
Cesare <cesare.concordia@isti.cnr.it>
2017-10-04 12:18:09 +0200

    license text added

--- src/main/java/eu/vre4eic/evre/telegram/updateshandlers/TgAuthenticator.java
+++ src/main/java/eu/vre4eic/evre/telegram/updateshandlers/TgAuthenticator.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2017 VRE4EIC Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package eu.vre4eic.evre.telegram.updateshandlers;
 
 //import org.slf4j.Logger;
@@ -103,7 +118,7 @@ public class TgAuthenticator extends  TelegramLongPollingCommandBot {
     @Override
     public String getBotToken() {
         
-        return "my_token";
+        return "304076530:AAFryi3D0xd7e6Vu_yyfiOpIMAPiHDAwIq0";
     }
 	
 
